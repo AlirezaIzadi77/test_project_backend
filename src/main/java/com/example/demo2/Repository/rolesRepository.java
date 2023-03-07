bfe1bcc7a536208b7e59e1aff2dba913c6074cd6
Alireza <alireza.izadi@gmail.com>
2023-03-07 19:02:10 +0330

    initial project

--- src/main/java/com/example/demo2/Repository/rolesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2.Repository;
-
-import com.example.demo2.Model.roles;
-import org.springframework.data.repository.CrudRepository;
-
-public interface rolesRepository extends CrudRepository<roles , Long> {
-
-}
